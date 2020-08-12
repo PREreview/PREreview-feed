722ddb074c8f3fe134a8a364aa95e17a2f13f8f6
Harum Helmy <harumendhah@gmail.com>
2020-08-12 15:45:16 -0400

    edited structure of XML feed

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const REVIEWS_API_URL =
 
 const USERS_API_URL = 'https://outbreaksci.prereview.org/api/role?q=*:*&include_docs=true';
 
+// only fetches preprints with reviews
+const PREPRINTS_API_URL = 'https://outbreaksci.prereview.org/api/preprint?'
+const REVIEWED_PREPRINTS_QUERY = 'q=nReviews%3A[1+TO+Infinity]&sort=[%22-score%3Cnumber%3E%22%2C%22-datePosted%3Cnumber%3E%22%2C%22-dateLastActivity%3Cnumber%3E%22]&include_docs=true&counts=[%22hasPeerRec%22%2C%22hasOthersRec%22%2C%22hasData%22%2C%22hasCode%22%2C%22hasReviews%22%2C%22hasRequests%22%2C%22subjectName%22]&ranges={%22nReviews%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}%2C%22nRequests%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}}'
+
 const getReviews = async (bookmark) => {
   let url = REVIEWS_API_URL 
 
@@ -28,6 +32,22 @@ const getUsers = async (bookmark) => {
 
   if (bookmark) {
     url = url + `&bookmark=${bookmark}`
+  }
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('oh dear, looks like we broke the users fetch: ', error);
+  }
+};
+
+const getPreprints = async (bookmark) => {
+  let url = PREPRINTS_API_URL + REVIEWED_PREPRINTS_QUERY
+
+  if (bookmark) {
+    url = PREPRINTS_API_URL + `bookmark=${bookmark}` + `&${REVIEWED_PREPRINTS_QUERY}`
   }
 
   try {
@@ -71,6 +91,22 @@ const getAllRoles = async () => {
   return results
 }
 
+const getAllPreprints = async () => {
+  let prevBookmark = null;
+  let results = [];
+  let prevRows = [];
+
+  do {
+    let { rows, bookmark } = await getPreprints(prevBookmark)
+    prevBookmark = bookmark
+    prevRows = rows
+    results = results.concat(rows)
+  }
+  while (prevRows.length > 0)
+
+  return results
+}
+
 const main = async () => {
   const roles = await getAllRoles()
   const rolesMap = roles.reduce((rolesMap, role)=>{
@@ -85,45 +121,66 @@ const main = async () => {
       preprintId: review.doc.object.doi ? review.doc.object.doi : review.doc.object.arXivId,
       preprintTitle: review.doc.object.name,
       idType: review.doc.object.doi ? 'DOI' : 'arXivId',
-      server: review.doc.object.preprintServer ? review.doc.object.preprintServer.name : 'Unable to parse preprint server',
       reviewer: rolesMap.get(review.doc.agent), 
       dateReviewed: review.doc.startTime,
       reviewLink: `https://outbreaksci.prereview.org/${review.doc.object.doi ? review.doc.object.doi : review.doc.object.arXivId}?role=${review.doc.agent.split(':')[1]}`
     }
   })
 
+
+
   let reviews = JSON.stringify(cleaned)
   
   fs.writeFile('reviews.txt', reviews, (error) => {
     if (error) throw error;
-    console.log('Reviews have been saved to a file called reviews.txt');
+    console.log('All reviews have been saved to a file called reviews.txt');
   })
 
   return cleaned
 }
 
+main()
+
+const processPreprints = async () => {
+  const hasReviews = await getAllPreprints()
+  const withDOI = hasReviews.filter(preprint => !!preprint.doc.doi)
+  
+  const processed = withDOI.map(preprint => {
+    return {
+      title: preprint.doc.name,
+      doi: preprint.doc.doi,
+      link: `https://outbreaksci.prereview.org/${preprint.doc.doi}`
+    }
+  })
+
+  return processed
+}
+
+
 const buildXML = async () => {
-  let data = await main();
+  const preprints = await processPreprints()
 
   const xml = create({ version: '1.0' })
     .ele('links')
 
-   for (let i = 0; i < data.length; i++) {
+   for (let i = 0; i < preprints.length; i++) {
      xml.ele('link', { providerId: 'PREReview'})
       .ele('resource')
-        .ele('url').txt(data[i].reviewLink).up()
+        .ele('title').txt(`PREreview(s) for '${preprints[i].title}'`).up()
+        .ele('url').txt(preprints[i].link).up()
       .up()
       .ele('record')
-        .ele('source').txt(data[i].server).up()
-        .ele('id').txt(data[i].preprintId).up()
+        .ele('source').txt('DOI').up()
+        .ele('id').txt(preprints[i].doi).up()
       .up()
     .up()
    }
   
   xml.end({ prettyPrint: true});
+
   fs.writeFile('feed.xml', xml, (error) => {
     if (error) throw error;
-    console.log('Reviews have been saved to a file called feed.xml'); 
+    console.log('Links to preprints with reviews on PREreview have been saved to a file called feed.xml'); 
   })
 }
 

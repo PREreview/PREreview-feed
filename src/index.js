fed0885f79ae26c393424561cf90925a53417433
Josh King <jking@chambana.net>
2020-08-25 13:20:13 -0400

    Make filenames more consistent and add them to .gitignore, fix pretty printing of output files.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
-const fetch = require('node-fetch')
-const fs = require('fs')
-const { create } = require('xmlbuilder2')
+const fetch = require('node-fetch');
+const fs = require('fs');
+const { create } = require('xmlbuilder2');
 const Feed = require('feed').Feed;
 
 const REVIEWS_API_URL =
   'https://outbreaksci.prereview.org/api/action?q=@type:RapidPREreviewAction&include_docs=true';
 
-const USERS_API_URL = 'https://outbreaksci.prereview.org/api/role?q=*:*&include_docs=true';
+const USERS_API_URL =
+  'https://outbreaksci.prereview.org/api/role?q=*:*&include_docs=true';
 
 // only fetches preprints with reviews
-const PREPRINTS_API_URL = 'https://outbreaksci.prereview.org/api/preprint?'
-const REVIEWED_PREPRINTS_QUERY = 'q=nReviews%3A[1+TO+Infinity]&sort=[%22-score%3Cnumber%3E%22%2C%22-datePosted%3Cnumber%3E%22%2C%22-dateLastActivity%3Cnumber%3E%22]&include_docs=true&counts=[%22hasPeerRec%22%2C%22hasOthersRec%22%2C%22hasData%22%2C%22hasCode%22%2C%22hasReviews%22%2C%22hasRequests%22%2C%22subjectName%22]&ranges={%22nReviews%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}%2C%22nRequests%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}}'
+const PREPRINTS_API_URL = 'https://outbreaksci.prereview.org/api/preprint?';
+const REVIEWED_PREPRINTS_QUERY =
+  'q=nReviews%3A[1+TO+Infinity]&sort=[%22-score%3Cnumber%3E%22%2C%22-datePosted%3Cnumber%3E%22%2C%22-dateLastActivity%3Cnumber%3E%22]&include_docs=true&counts=[%22hasPeerRec%22%2C%22hasOthersRec%22%2C%22hasData%22%2C%22hasCode%22%2C%22hasReviews%22%2C%22hasRequests%22%2C%22subjectName%22]&ranges={%22nReviews%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}%2C%22nRequests%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}}';
 
 const getReviews = async (bookmark) => {
-  let url = REVIEWS_API_URL 
+  let url = REVIEWS_API_URL;
 
   if (bookmark) {
-    url = url + `&bookmark=${bookmark}`
+    url = url + `&bookmark=${bookmark}`;
   }
 
   try {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log('oh dear, looks like we broke the reviews fetch: ', error)
+    console.log('oh dear, looks like we broke the reviews fetch: ', error);
   }
-}
+};
 
 const getUsers = async (bookmark) => {
-  let url = USERS_API_URL 
+  let url = USERS_API_URL;
 
   if (bookmark) {
-    url = url + `&bookmark=${bookmark}`
+    url = url + `&bookmark=${bookmark}`;
   }
 
   try {
@@ -45,10 +47,13 @@ const getUsers = async (bookmark) => {
 };
 
 const getPreprints = async (bookmark) => {
-  let url = PREPRINTS_API_URL + REVIEWED_PREPRINTS_QUERY
+  let url = PREPRINTS_API_URL + REVIEWED_PREPRINTS_QUERY;
 
   if (bookmark) {
-    url = PREPRINTS_API_URL + `bookmark=${bookmark}` + `&${REVIEWED_PREPRINTS_QUERY}`
+    url =
+      PREPRINTS_API_URL +
+      `bookmark=${bookmark}` +
+      `&${REVIEWED_PREPRINTS_QUERY}`;
   }
 
   try {
@@ -66,15 +71,14 @@ const getAllReviews = async () => {
   let prevRows = [];
 
   do {
-   let { rows, bookmark } = await getReviews(prevBookmark)
-   prevBookmark = bookmark
-   prevRows = rows
-   results = results.concat(rows)
-  }
-  while (prevRows.length > 0)
+    let { rows, bookmark } = await getReviews(prevBookmark);
+    prevBookmark = bookmark;
+    prevRows = rows;
+    results = results.concat(rows);
+  } while (prevRows.length > 0);
 
-  return results
-}
+  return results;
+};
 
 const getAllRoles = async () => {
   let prevBookmark = null;
@@ -82,15 +86,14 @@ const getAllRoles = async () => {
   let prevRows = [];
 
   do {
-    let { rows, bookmark } = await getUsers(prevBookmark)
-    prevBookmark = bookmark
-    prevRows = rows
-    results = results.concat(rows)
-  }
-  while (prevRows.length > 0)
+    let { rows, bookmark } = await getUsers(prevBookmark);
+    prevBookmark = bookmark;
+    prevRows = rows;
+    results = results.concat(rows);
+  } while (prevRows.length > 0);
 
-  return results
-}
+  return results;
+};
 
 const getAllPreprints = async () => {
   let prevBookmark = null;
@@ -98,69 +101,80 @@ const getAllPreprints = async () => {
   let prevRows = [];
 
   do {
-    let { rows, bookmark } = await getPreprints(prevBookmark)
-    prevBookmark = bookmark
-    prevRows = rows
-    results = results.concat(rows)
-  }
-  while (prevRows.length > 0)
+    let { rows, bookmark } = await getPreprints(prevBookmark);
+    prevBookmark = bookmark;
+    prevRows = rows;
+    results = results.concat(rows);
+  } while (prevRows.length > 0);
 
-  return results
-}
+  return results;
+};
 
 const main = async () => {
-  const roles = await getAllRoles()
-  const rolesMap = roles.reduce((rolesMap, role)=>{
-     rolesMap.set(role.id, role.doc['@type'] === 'AnonymousReviewerRole' ? 'Anonymous' : role.doc.name)
-     return rolesMap
-  }, new Map())
+  const roles = await getAllRoles();
+  const rolesMap = roles.reduce((rolesMap, role) => {
+    rolesMap.set(
+      role.id,
+      role.doc['@type'] === 'AnonymousReviewerRole'
+        ? 'Anonymous'
+        : role.doc.name,
+    );
+    return rolesMap;
+  }, new Map());
 
-  const allReviews = await getAllReviews()
+  const allReviews = await getAllReviews();
 
-  const cleaned = allReviews.map(review => {
+  const cleaned = allReviews.map((review) => {
     return {
-      preprintId: review.doc.object.doi ? review.doc.object.doi : review.doc.object.arXivId,
+      preprintId: review.doc.object.doi
+        ? review.doc.object.doi
+        : review.doc.object.arXivId,
       preprintTitle: review.doc.object.name,
       idType: review.doc.object.doi ? 'DOI' : 'arXivId',
-      reviewer: rolesMap.get(review.doc.agent), 
+      reviewer: rolesMap.get(review.doc.agent),
       dateReviewed: new Date(review.doc.startTime),
-      reviewLink: `https://outbreaksci.prereview.org/${review.doc.object.doi ? review.doc.object.doi : review.doc.object.arXivId}?role=${review.doc.agent.split(':')[1]}`
-    }
-  })
+      reviewLink: `https://outbreaksci.prereview.org/${
+        review.doc.object.doi
+          ? review.doc.object.doi
+          : review.doc.object.arXivId
+      }?role=${review.doc.agent.split(':')[1]}`,
+    };
+  });
 
-  const sorted = cleaned.sort((a, b) => b.dateReviewed - a.dateReviewed)
+  const sorted = cleaned.sort((a, b) => b.dateReviewed - a.dateReviewed);
 
-  const reviews = JSON.stringify(sorted)
-  
-  fs.writeFile('reviews.txt', reviews, (error) => {
+  const reviews = JSON.stringify(sorted, null, 2);
+
+  fs.writeFile('reviews.json', reviews, (error) => {
     if (error) throw error;
-    console.log('All reviews have been saved to a file called reviews.txt');
-  })
+    console.log('A JSON file of all reviews has been saved to reviews.json!');
+  });
 
-  return sorted
-}
+  return sorted;
+};
 
 const buildFeed = async () => {
-  const reviews = await main()
+  const reviews = await main();
 
-  const withDOI = reviews.filter(review => review.idType === 'DOI')
+  const withDOI = reviews.filter((review) => review.idType === 'DOI');
 
   const feed = new Feed({
-    title: "OutbreakScience Rapid PREreview",
-    description: "Rapid reviews of preprints posted on OutbreakScience Rapid PREreview",
-    id: "https://outbreaksci.prereview.org/",
-    link: "https://outbreaksci.prereview.org/",
-    image: "http://example.com/image.png",
-    favicon: "http://example.com/favicon.ico",
-    copyright: "OutbreakScience Rapid PREreview",
+    title: 'OutbreakScience Rapid PREreview',
+    description:
+      'Rapid reviews of preprints posted on OutbreakScience Rapid PREreview',
+    id: 'https://outbreaksci.prereview.org/',
+    link: 'https://outbreaksci.prereview.org/',
+    image: 'http://example.com/image.png',
+    favicon: 'http://example.com/favicon.ico',
+    copyright: 'OutbreakScience Rapid PREreview',
     author: {
-      name: "Outbreak Science Rapid PREreview",
-      email: "outbreaksci@prereview.org",
-      link: "https://outbreaksci.prereview.org/"
-    }
-  })
+      name: 'Outbreak Science Rapid PREreview',
+      email: 'outbreaksci@prereview.org',
+      link: 'https://outbreaksci.prereview.org/',
+    },
+  });
 
-  withDOI.forEach(review => {
+  withDOI.forEach((review) => {
     feed.addItem({
       title: 'A rapid review of ' + `${review.preprintTitle}`,
       preprintDOI: review.preprintId,
@@ -168,58 +182,73 @@ const buildFeed = async () => {
       link: review.reviewLink,
       reviewer: review.reviewer,
       date: review.dateReviewed,
-    })
-  })
-  
-  fs.writeFile('reviews.rss', feed.rss2(), (error) => {
-    if (error) throw error;
-    (console.log("Thanks for generating an RSS feed of all reviews of preprints with DOI on OutbreakScience Rapid PREreview!"));
-  })
-}
+    });
+  });
 
-buildFeed()
+  fs.writeFile('rss.xml', feed.rss2(), (error) => {
+    if (error) throw error;
+    console.log(
+      'An RSS feed of all reviews has been saved to a file called rss.xml!',
+    );
+  });
+};
+
+console.log(
+  'Downloading all reviews of preprints with DOIs on OutbreakScience Rapid PREreview...',
+);
+
+buildFeed();
 
 const processPreprints = async () => {
-  const hasReviews = await getAllPreprints()
-  const withDOI = hasReviews.filter(preprint => !!preprint.doc.doi)
-  
-  const processed = withDOI.map(preprint => {
+  const hasReviews = await getAllPreprints();
+  const withDOI = hasReviews.filter((preprint) => !!preprint.doc.doi);
+
+  const processed = withDOI.map((preprint) => {
     return {
       title: preprint.doc.name,
       doi: preprint.doc.doi,
-      link: `https://outbreaksci.prereview.org/${preprint.doc.doi}`
-    }
-  })
+      link: `https://outbreaksci.prereview.org/${preprint.doc.doi}`,
+    };
+  });
 
-  return processed
-}
-
+  return processed;
+};
 
 const buildXML = async () => {
-  const preprints = await processPreprints()
+  const preprints = await processPreprints();
 
-  const xml = create({ version: '1.0' })
-    .ele('links')
+  const xml = create({ version: '1.0' }).ele('links');
 
-   for (let i = 0; i < preprints.length; i++) {
-     xml.ele('link', { providerId: 'PREReview' })
+  for (let i = 0; i < preprints.length; i++) {
+    xml
+      .ele('link', { providerId: 'PREReview' })
       .ele('resource')
-        .ele('title').txt(`PREreview(s) of '${preprints[i].title}'`).up()
-        .ele('url').txt(preprints[i].link).up()
+      .ele('title')
+      .txt(`PREreview(s) of '${preprints[i].title}'`)
+      .up()
+      .ele('url')
+      .txt(preprints[i].link)
+      .up()
       .up()
       .ele('record')
-        .ele('source').txt('DOI').up()
-        .ele('id').txt(preprints[i].doi).up()
+      .ele('source')
+      .txt('DOI')
       .up()
-    .up()
-   }
-  
-  xml.end({ prettyPrint: true });
+      .ele('id')
+      .txt(preprints[i].doi)
+      .up()
+      .up()
+      .up();
+  }
 
-  fs.writeFile('feed.xml', xml, (error) => {
+  const output = xml.end({ prettyPrint: true });
+
+  fs.writeFile('europepmc.xml', output, (error) => {
     if (error) throw error;
-    console.log('Links to preprints with reviews on PREreview have been saved to a file called feed.xml'); 
-  })
-}
+    console.log(
+      'An XML file of all reviews in Europe PMC LabsLink format has been saved to europepmc.xml!',
+    );
+  });
+};
 
-buildXML()
+buildXML();

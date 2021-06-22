4a8141ddd0b8573bcef2d688e02bc47c8830c0af
Josh King <jking@chambana.net>
2021-06-22 15:47:22 -0400

    Update feed script to generate from new API.

--- src/index.js
+++ src/index.js
@@ -1,212 +1,80 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const { create } = require('xmlbuilder2');
-const Feed = require('feed').Feed;
+const ftp = require('basic-ftp');
 
-const REVIEWS_API_URL =
-  'https://outbreaksci.prereview.org/api/action?q=@type:RapidPREreviewAction&include_docs=true';
-
-const USERS_API_URL =
-  'https://outbreaksci.prereview.org/api/role?q=*:*&include_docs=true';
+let FTP_USER, FTP_PASS;
+let URL = 'http://prereview2-staging.azurewebsites.net';
+if (process.env.NODE_ENV === 'production') {
+  URL = 'https://prereview.org';
+  FTP_USER = process.env.FTP_USER;
+  FTP_PASS = process.env.FTP_PASS;
+}
 
 // only fetches preprints with reviews
-const PREPRINTS_API_URL = 'https://outbreaksci.prereview.org/api/preprint?';
-const REVIEWED_PREPRINTS_QUERY =
-  'q=nReviews%3A[1+TO+Infinity]&sort=[%22-score%3Cnumber%3E%22%2C%22-datePosted%3Cnumber%3E%22%2C%22-dateLastActivity%3Cnumber%3E%22]&include_docs=true&counts=[%22hasPeerRec%22%2C%22hasOthersRec%22%2C%22hasData%22%2C%22hasCode%22%2C%22hasReviews%22%2C%22hasRequests%22%2C%22subjectName%22]&ranges={%22nReviews%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}%2C%22nRequests%22%3A{%220%22%3A%22[0+TO+1}%22%2C%221%2B%22%3A%22[1+TO+Infinity]%22%2C%222%2B%22%3A%22[2+TO+Infinity]%22%2C%223%2B%22%3A%22[3+TO+Infinity]%22%2C%224%2B%22%3A%22[4+TO+Infinity]%22%2C%225%2B%22%3A%22[5+TO+Infinity]%22}}';
+const PREPRINTS_API_URL = `${URL}/api/v2/preprints`;
+const PUBLISHED_PREPRINTS_QUERY = '';
 
-const getReviews = async (bookmark) => {
-  let url = REVIEWS_API_URL;
-
-  if (bookmark) {
-    url = url + `&bookmark=${bookmark}`;
-  }
+const getPreprints = async () => {
+  let url = PREPRINTS_API_URL + PUBLISHED_PREPRINTS_QUERY;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log('oh dear, looks like we broke the reviews fetch: ', error);
+    console.log('oh dear, looks like we broke the preprints fetch: ', error);
   }
 };
 
-const getUsers = async (bookmark) => {
-  let url = USERS_API_URL;
-
-  if (bookmark) {
-    url = url + `&bookmark=${bookmark}`;
+const decodeHandle = (value) => {
+  if (!value) {
+    throw new Error('You must provide a preprintId to decode');
   }
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log('oh dear, looks like we broke the users fetch: ', error);
-  }
-};
-
-const getPreprints = async (bookmark) => {
-  let url = PREPRINTS_API_URL + REVIEWED_PREPRINTS_QUERY;
-
-  if (bookmark) {
-    url =
-      PREPRINTS_API_URL +
-      `bookmark=${bookmark}` +
-      `&${REVIEWED_PREPRINTS_QUERY}`;
+  let scheme;
+  if (value.startsWith('doi')) {
+    scheme = 'doi';
+  } else if (value.startsWith('arxiv')) {
+    scheme = 'arxiv';
   }
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log('oh dear, looks like we broke the users fetch: ', error);
-  }
-};
-
-const getAllReviews = async () => {
-  let prevBookmark = null;
-  let results = [];
-  let prevRows = [];
-
-  do {
-    let { rows, bookmark } = await getReviews(prevBookmark);
-    prevBookmark = bookmark;
-    prevRows = rows;
-    results = results.concat(rows);
-  } while (prevRows.length > 0);
-
-  return results;
-};
-
-const getAllRoles = async () => {
-  let prevBookmark = null;
-  let results = [];
-  let prevRows = [];
-
-  do {
-    let { rows, bookmark } = await getUsers(prevBookmark);
-    prevBookmark = bookmark;
-    prevRows = rows;
-    results = results.concat(rows);
-  } while (prevRows.length > 0);
-
-  return results;
-};
-
-const getAllPreprints = async () => {
-  let prevBookmark = null;
-  let results = [];
-  let prevRows = [];
-
-  do {
-    let { rows, bookmark } = await getPreprints(prevBookmark);
-    prevBookmark = bookmark;
-    prevRows = rows;
-    results = results.concat(rows);
-  } while (prevRows.length > 0);
-
-  return results;
-};
-
-const main = async () => {
-  const roles = await getAllRoles();
-  const rolesMap = roles.reduce((rolesMap, role) => {
-    rolesMap.set(
-      role.id,
-      role.doc['@type'] === 'AnonymousReviewerRole'
-        ? 'Anonymous'
-        : role.doc.name,
+  if (!scheme) {
+    throw new Error(
+      'String is not an encoded preprint ID (could not extract scheme)',
     );
-    return rolesMap;
-  }, new Map());
+  }
 
-  const allReviews = await getAllReviews();
-
-  const cleaned = allReviews.map((review) => {
-    return {
-      preprintId: review.doc.object.doi
-        ? review.doc.object.doi
-        : review.doc.object.arXivId,
-      preprintTitle: review.doc.object.name,
-      idType: review.doc.object.doi ? 'DOI' : 'arXivId',
-      reviewer: rolesMap.get(review.doc.agent),
-      dateReviewed: new Date(review.doc.startTime),
-      reviewLink: `https://outbreaksci.prereview.org/${
-        review.doc.object.doi
-          ? review.doc.object.doi
-          : review.doc.object.arXivId
-      }?role=${review.doc.agent.split(':')[1]}`,
-    };
-  });
-
-  const sorted = cleaned.sort((a, b) => b.dateReviewed - a.dateReviewed);
-
-  const reviews = JSON.stringify(sorted, null, 2);
-
-  fs.writeFile('reviews.json', reviews, (error) => {
-    if (error) throw error;
-    console.log('A JSON file of all reviews has been saved to reviews.json!');
-  });
-
-  return sorted;
+  return {
+    id: `${value.split(':')[1]}`,
+    scheme: scheme,
+  };
 };
-
-const buildFeed = async () => {
-  const reviews = await main();
-
-  const withDOI = reviews.filter((review) => review.idType === 'DOI');
-
-  const feed = new Feed({
-    title: 'OutbreakScience Rapid PREreview',
-    description:
-      'Rapid reviews of preprints posted on OutbreakScience Rapid PREreview',
-    link: 'https://outbreaksci.prereview.org/',
-    copyright: 'OutbreakScience Rapid PREreview',
-    author: {
-      name: 'Outbreak Science Rapid PREreview',
-      email: 'outbreaksci@prereview.org',
-      link: 'https://outbreaksci.prereview.org/',
-    },
-  });
-
-  withDOI.forEach((review) => {
-    feed.addItem({
-      title: 'A rapid review of ' + `${review.preprintTitle} by ${review.reviewer}` ,
-      preprintDOI: review.preprintId, 
-      link: review.reviewLink,
-      date: review.dateReviewed,
-    });
-  });
-
-  fs.writeFile('rss.xml', feed.rss2(), (error) => {
-    if (error) throw error;
-    console.log(
-      'An RSS feed of all reviews has been saved to a file called rss.xml!',
-    );
-  });
-};
-
-console.log(
-  'Downloading all reviews of preprints with DOIs on Outbreak Science Rapid PREreview...',
-);
-
-buildFeed();
 
 const processPreprints = async () => {
-  const hasReviews = await getAllPreprints();
-  const withDOI = hasReviews.filter((preprint) => !!preprint.doc.doi);
+  const hasReviews = await getPreprints();
+  return hasReviews.reduce((withDOI, preprint) => {
+    if (!preprint.handle) return withDOI;
 
-  const processed = withDOI.map((preprint) => {
-    return {
-      title: preprint.doc.name,
-      doi: preprint.doc.doi,
-      link: `https://outbreaksci.prereview.org/${preprint.doc.doi}`,
-    };
-  });
+    let id, scheme;
+    try {
+      ({ id, scheme } = decodeHandle(preprint.handle));
+    } catch (err) {
+      console.error('Error parsing preprint handle:', err);
+      return withDOI;
+    }
 
-  return processed;
+    // Found a DOI
+    if (id && scheme === 'doi') {
+      withDOI.push({
+        title: preprint.title,
+        doi: id,
+        link: `${URL}/preprints/doi-${id.replace('/', '-')}`,
+      });
+    }
+
+    return withDOI;
+  }, []);
 };
 
 const buildXML = async () => {
@@ -219,17 +87,14 @@ const buildXML = async () => {
       .ele('link', { providerId: '2068' })
       .ele('resource')
       .ele('title')
-      .txt(`Rapid PREreview(s) of '${preprints[i].title}'`)
+      .txt(`PREreview(s) of '${preprints[i].title}'`)
       .up()
       .ele('url')
       .txt(preprints[i].link)
       .up()
       .up()
       .ele('record')
-      .ele('source')
-      .txt('DOI')
-      .up()
-      .ele('id')
+      .ele('doi')
       .txt(preprints[i].doi)
       .up()
       .up()
@@ -246,4 +111,29 @@ const buildXML = async () => {
   });
 };
 
-buildXML();
+const uploadToEuropePMC = async () => {
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+  try {
+    await client.access({
+      host: 'TBD',
+      user: `${FTP_USER}`,
+      password: `${FTP_PASS}`,
+      secure: false,
+    });
+    console.log(await client.list());
+    await client.uploadFrom('europepmc.xml', 'europepmc.xml');
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+};
+
+buildXML()
+  .then(() => {
+    if (FTP_USER && FTP_PASS) {
+      uploadToEuropePMC();
+    }
+    return;
+  })
+  .catch((err) => console.error('Failed to export:', err));
